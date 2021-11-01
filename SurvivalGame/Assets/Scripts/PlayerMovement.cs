62568eecd8514ec0693e542d246e217bba1281ba
Something226 <axel.lieback@gmail.com>
2021-11-01 23:53:50 +0100

    Added movement and follow script

--- SurvivalGame/Assets/Scripts/PlayerMovement.cs
+++ SurvivalGame/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,29 @@
-using System.Numerics;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    public float movementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidBody.velocity.y) < 0.001f)
+        {
+            _rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
 
     }
 }
