ccd7672096b70012315213c324e221da308f1ef8
Steomon <38396746+Steomon@users.noreply.github.com>
2021-11-12 09:40:11 +0100

    more problems with BlockModifications

--- SurvivalGame/Assets/Scripts/BlockModification.cs
+++ SurvivalGame/Assets/Scripts/BlockModification.cs
@@ -5,17 +5,19 @@ using UnityEngine.Tilemaps;
 
 public class BlockModification : MonoBehaviour
 {
-    private Tilemap Tilemap;
+    private Tilemap tilemap;
 
     // Update is called once per frame
     void Update()
     {
         //when left click is pressed 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int coordinate = Tilemap.WorldToCell(mouseWorldPos);
-            Tilemap.SetTile(coordinate, null);
+            Vector3Int coordinate = tilemap.WorldToCell(mouseWorldPos);
+            tilemap.SetTile(coordinate, null);
+            Debug.Log(mouseWorldPos);
+            Debug.Log(coordinate);
         }
         //when right click is pressed
         /*        if (Input.GetMouseButtonDown(1))
